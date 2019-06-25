1c1352e709ed0f0843b0564d3fb7a54a1c6ed3de
kevinwang09 <kevin.wang@sydney.edu.au>
2019-06-25 15:15:19 +0800

    upd igraph_mcvis docs

--- R/igraph_mcvis.R
+++ R/igraph_mcvis.R
@@ -1,9 +1,9 @@
 #' @author Chen Lin, Kevin Wang, Samuel Mueller
 #' @title Multi-collinearity Visualization
 #' @param mcvis_result Output of the mcvis function
-#' @param tau A parameter determining below which thickness the plotting lines hide.
+#' @param thres A parameter determining below which thickness the plotting lines hide.
 #' @param eig.max The maximum number of eigenvalues to be displayed on the plot.
-#' @param vol.max The maximum number of variables to be displayed on the plot.
+#' @param var.max The maximum number of variables to be displayed on the plot.
 #' @import igraph
 #' @export
 #' @examples
@@ -17,9 +17,9 @@
 
 
 igraph_mcvis <- function(mcvis_result,
-                         tau = 2/3,
+                         thres = 2/3,
                          eig.max = ncol(mcvis_result$MC),
-                         vol.max = ncol(mcvis_result$MC))
+                         var.max = ncol(mcvis_result$MC))
 {
   #####################
   g = 1-mcvis_result$MC
@@ -28,21 +28,21 @@ igraph_mcvis <- function(mcvis_result,
 
   p = ncol(g)
   eig.max = min(p, eig.max)
-  vol.max = min(p, vol.max)
+  var.max = min(p, var.max)
   or = order(g[p,]) ## Order the columns of g by the smallest eigenvalue
-  or = or[1:vol.max]
+  or = or[1:var.max]
   g.or = g[,or]
-  if (vol.max > 1) {g.or = g.or[p:(p-eig.max+1),]} else {g.or = as.matrix(g.or[p:(p-eig.max+1)])}
+  if (var.max > 1) {g.or = g.or[p:(p-eig.max+1),]} else {g.or = as.matrix(g.or[p:(p-eig.max+1)])}
   if (eig.max == 1) {g.or = t(g.or)}
 
  ######################
   ## igraph plotting
-  g.or[g.or > 1-tau/p] = 1
+  g.or[g.or > 1-thres/p] = 1
   ## For plotting purpose, if the values of g.or is above a certain threshold, then we set it to 1
   col.names = col.names[or]
   ## reorder the variables by the connection with the smallest eigenvalue
 
-  M = matrix(1,eig.max,vol.max)
+  M = matrix(1,eig.max,var.max)
   G = igraph::graph_from_incidence_matrix(M)
 
   vec = as.vector(t(g.or))
@@ -64,9 +64,9 @@ igraph_mcvis <- function(mcvis_result,
        main="mcvis"
   )
 
-  if (vol.max>1)
-  {text(x=rep(1.7,vol.max),y=(1:vol.max)*2/(1-vol.max)+(vol.max+1)/(vol.max-1),G.text)}
+  if (var.max>1)
+  {text(x=rep(1.7,var.max),y=(1:var.max)*2/(1-var.max)+(var.max+1)/(var.max-1),G.text)}
 
-  if (vol.max==1) {text(x=2,y=0,G.text)}
+  if (var.max==1) {text(x=2,y=0,G.text)}
   text(x=0,y=-1.4,"tau1: the inverse of the smallest eigenvalue")
 }
